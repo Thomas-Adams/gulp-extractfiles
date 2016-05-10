93654e001ae21bc7413623d066107573bd5f80f8
heinrich.adams@gmail.com <heinrich.adams@gmail.com>
2016-05-10 18:11:07 +0800

    clean up project, delete unnecessary files, update package.json

--- public/_common/js/src/main-pagetransitions-ioloader.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $(".content").children().removeClass("show");
-    $('.focuspoint').focusPoint();
-    var wh = window.innerHeight;
-    $("section").height(wh);
-    $("img.lazy").ioloader();
-});
